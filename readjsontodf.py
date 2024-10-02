ac0350e1688eb9f12a4990b372480425b4b63b85
Phillip Andrew Raywood <thephillipraywood@gmail.com>
2024-10-02 18:27:11 -0400

    Update readjsontodf.py
    call json method of DataFrameReader, show

--- readjsontodf.py
+++ readjsontodf.py
@@ -1,1 +1,15 @@
+import sys
 
+from pyspark.sql import DataFrame, SparkSession
+
+from typing import List
+
+def main(spark: SparkSession, *args: str) -> None:
+    source = args[0]
+    df = spark.read.json(source)
+    df.show()
+    
+if __name__ == "__main__":
+    spark: SparkSession= SparkSession.builder.master("local[*]").getOrCreate()
+    args: List[str] = sys.argv[1:]
+    main(spark, args)
