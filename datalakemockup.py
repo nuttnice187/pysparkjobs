0765ad1751ad99fc92a9a243c02420c871e55b29
Phillip Andrew Raywood <thephillipraywood@gmail.com>
2024-10-02 18:10:36 -0400

    Update datalakemockup.py
    read github csv and write to firectory given in argument vector

--- datalakemockup.py
+++ datalakemockup.py
@@ -1,18 +1,25 @@
 import sys
-from datetime import date, datetime
-from pyspark.sql import Row, SparkSession
+
+from pyspark import SparkContext, SparkFiles
+
+from pyspark.sql import DataFrameWriter, Row, SparkSession
+
+from pyspark.sql.functions import monotonically_increasing_id
+
 from typing import List
 
 def main(spark: SparkSession, *args: str) -> None:
-    df = spark.createDataFrame([
-        Row(a=1, b=2., c='string1', d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
-        Row(a=2, b=3., c='string2', d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
-        Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0))
-    ])
-    df.show()
-    print(args)
+    URL, destination, mode = args[0]
+    sc: SparkContext= spark.sparkContext
+    sc.addFile(URL)
+    file_name: str= SparkFiles.get('weight-height.csv')
+    spark.read.csv(file_name, header=True, inferSchema=True) \
+        .select(monotonically_increasing_id().alias('Id'), '*') \
+        .write \
+        .partitionBy('Id') \
+        .json(destination, mode)
     
 if __name__ == "__main__":
     spark: SparkSession= SparkSession.builder.master("local[*]").getOrCreate()
-    args: List[str] = sys.argv
+    args: List[str] = sys.argv[1:]
     main(spark, args)
