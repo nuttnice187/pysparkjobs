4be6d6cea044c10093c1e93cdd66959ab3fecb7d
Phillip Andrew Raywood <thephillipraywood@gmail.com>
2024-10-02 17:07:56 -0400

    Update datalakemockup.py
    show test df

--- datalakemockup.py
+++ datalakemockup.py
@@ -1,8 +1,15 @@
 import sys
-from pyspark.sql import SparkSession
+from datetime import date, datetime
+from pyspark.sql import Row, SparkSession
 from typing import List
 
-def main(spark: SparkSession, *args: str):
+def main(spark: SparkSession, *args: str) -> None:
+    df = spark.createDataFrame([
+        Row(a=1, b=2., c='string1', d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
+        Row(a=2, b=3., c='string2', d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
+        Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0))
+    ])
+    df.show()
     print(args)
     
 if __name__ == "__main__":
